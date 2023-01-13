11cb868b9ca36a32f3bb0dbe0579ef4200f1beb7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-01-13 14:19:26 +0200

    Second  thread  added

---
+++ Multithreading/IsAlive/Two.java
@@ -0,0 +1,25 @@
+package Multithreading.IsAlive;
+
+public class Two extends Thread {
+
+    private final String name;
+
+    public Two(String name){
+        super(name);
+        this.name = name;
+        System.out.println(name + ": created");
+        start();
+    }
+
+    @Override
+    public void run(){
+        for (int i = 5 ; i > 0 ; i--){
+            try {
+                System.out.println(name + ": " + i);
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
