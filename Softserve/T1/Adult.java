a8f62c0495113f94801acb403553cad6aaaffd69
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-01-16 17:54:08 +0200

    Update

--- Softserve/T1/Adult.java
+++ Softserve/T1/Adult.java
@@ -3,7 +3,8 @@ package Softserve.T1;
 public class Adult extends Person{
 
     public Adult(String name,String healthInfo,int age,int passportNumber){
-        super(name,healthInfo,age,passportNumber);
+        super(name,healthInfo,age);
+        this.passportNumber = passportNumber;
     }
 
     @Override
--- Softserve/T1/Person.java
+++ Softserve/T1/Person.java
@@ -11,11 +11,6 @@ public abstract class Person {
         this.age = age;
     }
 
-    public Person(String name, String healthInfo, int age, int passportNumber) {
-        this.name = name;
-        this.healthInfo = healthInfo;
-        this.age = age;
-        this.passportNumber = passportNumber;
-    }
+
     public abstract String getHealthInfo();
 }
