354af366c89ae53f57ecad05a65d000dedec035f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-01-17 03:43:47 +0200

    Coffee added

--- Softserve/T2/V2/Coffee.java
+++ Softserve/T2/V2/Coffee.java
@@ -3,7 +3,7 @@ package Softserve.T2.V2;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Coffee {
+public abstract class Coffee implements DrinkReceipt,DrinkPreparation,Rating {
     protected String name;
     protected int rating;
     protected Map<String,Integer> ingridients = new HashMap<>();
@@ -14,4 +14,20 @@ public class Coffee {
 
     }
 
+    @Override
+    public int getRating() {
+        return rating;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+
+    @Override
+    public abstract DrinkReceipt addComponent(String componentName, int componentCount);
+
+    @Override
+    public abstract Map<String, Integer> makeDrink();
 }
